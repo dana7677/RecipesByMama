abc176b3f0b7fac75f0dc8aa3d80e812343a79a9
usertar <danote76772@gmail.com>
2026-01-22 21:08:53 +0100

    CreandoDiferentesActivitys

--- app/src/main/java/com/example/recipesbymama/MainActivity.kt
+++ app/src/main/java/com/example/recipesbymama/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.recipesbymama
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,10 +11,14 @@ import com.example.recipesbymama.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         binding= ActivityMainBinding.inflate(layoutInflater);
+
 
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -20,5 +26,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        setbBtnStart();
+
     }
+    public fun setbBtnStart()
+    {
+        binding.btnMyRecipes.setOnClickListener {
+
+            val intent: Intent = Intent(this, MyRecipes::class.java)
+            startActivity(intent);
+
+        };
+        binding.btnNewRecipe.setOnClickListener {
+
+            val intent: Intent = Intent(this, NewRecipe::class.java)
+            startActivity(intent);
+
+        };
+        binding.btnWeeklyDiet.setOnClickListener {
+            val intent: Intent = Intent(this, WeeklyDiet::class.java)
+            startActivity(intent);
+
+        }
+
+    }
+
 }
